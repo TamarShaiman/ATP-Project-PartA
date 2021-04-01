8ea4514ac8c7a6c380f1197f44692f69cc103827
TamarShaiman <75303340+TamarShaiman@users.noreply.github.com>
2021-04-01 11:52:53 +0300

    using prim
    prim

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,17 @@
 package com.company;
 
+import algorithms.mazeGenerators.EmptyMazeGenerator;
+import algorithms.mazeGenerators.Maze;
+import algorithms.mazeGenerators.MyMazeGenerator;
+
 import java.sql.SQLOutput;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("I love you");
-
+        MyMazeGenerator MMG = new MyMazeGenerator();
+        System.out.println(MMG.measureAlgorithmTimeMillis(10,10));
+        Maze MMaze = MMG.generate(10, 10);
+        MMaze.print();
     }
 }
